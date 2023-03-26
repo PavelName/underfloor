8e7718b94b63bbb43e028c4e85d9a0820df4285a
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-03-26 17:05:33 +0800

    corrected

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
-new Swiper('.hero__slider', {
+
+ 
+ new Swiper('.hero__slider', {
     slidesPerView: 2, 
     spaceBetween: 10,
     loop: true,
@@ -18,6 +20,36 @@ new Swiper('.hero__slider', {
     560: {
         spaceBetween: 8,
     }
+   }
+});
+
+// 
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-square');
+const totalPrice = document.querySelector('.js-total-price');
+const calcResultWrapper = document.querySelector('.calc__result-wrapper')
+ 
+const tariff = {
+economy: 500,
+comfort:550 ,
+premium:700,    
+};
+
+
+calcForm.addEventListener('submit', (event) => {
+ event.preventDefault();
+  
+
+   if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+        const squere = calcForm.width.value * calcForm.length.value;
+        const price = squere * tariff[calcForm.tariff.value];
+
+        calcResultWrapper.style.display = 'block';
+
+        totalSquare.textContent =`${squere} кв.м`;
+        totalPrice.textContent =`${price} rub.`;
+
    }
 });
 
